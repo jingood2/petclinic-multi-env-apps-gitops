fd0285bd442a8e52739aeb63eb939733bc1742ce
Kim Jinyoung <jingood2@gmail.com>
2023-04-24 04:56:03 +0900

    Delete docker-secret.sh

--- docker-secret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl create secret docker-registry dockerconfigjson -n staging \
---docker-server="https://index.docker.io/v1/" \
---docker-username=jingood2 \
---docker-password=dckr_pat_QlF3WsJXVVsyeMx5yMfocedbmQo \
---docker-email=jingood2@gmail.com
